24d70d77f9323c0f9345b594f0b3f22a7c49db9b
William <86013044+WilliamVSan@users.noreply.github.com>
2022-03-28 22:07:37 -0300

    Update TemaRepository.java

--- src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.blogpessoal.model.Tema;
 
+@Repository
 public interface TemaRepository extends JpaRepository<Tema, Long>{
 	
 	public List <Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
