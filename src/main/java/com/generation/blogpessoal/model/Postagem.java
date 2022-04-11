0bcd64fc1b1a768a39280941dc3c33ed7781b53c
WilliamVSan <williamvsantos75@gmail.com>
2022-04-11 12:30:18 -0300

    Atualizando.

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -39,7 +39,7 @@ public class Postagem {
 	private Tema tema;
 
 	@ManyToOne
-	@JsonIgnoreProperties("usuario")
+	@JsonIgnoreProperties("postagem")
 	private Usuario usuario;
 
 	public Long getId() {
