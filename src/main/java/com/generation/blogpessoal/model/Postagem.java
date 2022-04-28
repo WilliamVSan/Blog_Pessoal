5550dca829e527987efe3fc10d3a19f87802efcf
WilliamVSan <williamvsantos75@gmail.com>
2022-04-28 16:41:13 -0300

    Adicionando Get e Set do Usuario.

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -83,4 +83,11 @@ public class Postagem {
 		this.tema = tema;
 	}
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 }
